7e898b4da64430abc57bba846f553e5cb7c73259
Sasha <sasha41187@gmail.com>
2025-03-18 12:31:20 +0300

    Соловьев А : исправлен свайпер и начата секция новинок

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -4,16 +4,20 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-
-new Swiper(".swiper-main", {
+const swiperSection = document.querySelector(".swiper-main")
+if (swiperSection.querySelector(".swiper")){
+const mainSwiper = swiperSection.querySelector(".swiper")
+new Swiper(mainSwiper, {
     pagination : {
-        el : ".main-swiper-pagination",
+        el : ".swiper-main__pagination",
         clickable : true
     },
     navigation: {
-        nextEl: '.main-swiper-button-next',
-        prevEl: '.main-swiper-button-prev',
+        nextEl: '.swiper-main__button-next',
+        prevEl: '.swiper-main__button-prev',
       },
     slidesPerView: 1,
     // autoHeight : true
+    spaceBetween : 20,
 })
+}
