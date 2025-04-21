3b7327c398b91f9637604a37ee94dbb85d695d81
Sasha <sasha41187@gmail.com>
2025-04-21 16:10:14 +0300

    SolovevA

--- app/js/hidingHeader.js
+++ app/js/hidingHeader.js
@@ -1,31 +1,31 @@
 let lastScroll = 0;
 const header = document.querySelector('.header');
+if (header) {
 const headerTop = header.querySelector(".header__up").offsetHeight
 const headerOffSet = header.offsetHeight;
-
+const wrapperBottom = header.querySelector(".header__wrapper-bottom")
 const hideScroll = () => {
+  console.log(wrapperBottom)
   const burger = header.querySelector('.burger__modal');
   if (burger.classList.contains('burger__modal--active')) {
     header.classList.add('header__fixed--visible');
     header.classList.remove('header__fixed--hidden');
-  } else {
-    header.classList.add('header__fixed--hidden');
   }
-
-  if (window.scrollY > headerOffSet + 150) {
+  if (window.scrollY > headerOffSet + 150 && window.scrollY > lastScroll) {
+    header.classList.add('header__fixed--hidden');
     header.classList.add('header__fixed');
-    !burger.classList.contains('burger__modal--active') == true ? header.classList.add('header__fixed--hidden') : null;
+    burger.classList.contains('burger__modal--active') == true ? header.classList.remove('header__fixed--hidden') : null;
     document.querySelector('body').style = `padding-top : ${headerOffSet}`;
 
-    if (window.scrollY > lastScroll || burger.classList.contains('burger__modal--active')) {
-      header.classList.remove('header__fixed--visible');
-    } else if (window.scrollY < lastScroll) {
-      header.classList.add('header__fixed--visible');
-    }
-  } else {
+    
+  } 
+  else if (window.scrollY < lastScroll) {
+    header.classList.remove('header__fixed--hidden');
+  }
+
+  if (window.scrollY < headerOffSet + 150) {
     header.classList.remove('header__fixed');
     header.classList.remove('header__fixed--hidden');
-    header.classList.remove('header__fixed--visible');
     document.querySelector('body').style = `padding-top : ${0}`;
   }
 
@@ -35,3 +35,4 @@ window.addEventListener('scroll', () => {
   hideScroll();
 });
 hideScroll();
+}
